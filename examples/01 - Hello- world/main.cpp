239f53bf8485bb6a5c13e8bfe5c21d0ce6e20aa2
Sygmei <Mizugola@gmail.com>
2017-05-17 13:38:41 +0200

    Update main.cpp

--- examples/01 - Hello- world/main.cpp
+++ examples/01 - Hello- world/main.cpp
@@ -3,7 +3,22 @@
 
 using vili::DataParser;
 
+/*
+This example shows :
+  - How to parse a file
+  - How to get a simple BaseAttribute with 3 different ways
+*/
 int main(int argc, char** argv)
 {
-  
+    DataParser file("hello.vili"); //Creates a DataParser object and parse the file "hello.vili"
+    std::string message; //String where we will store message content
+    
+    //at (From DataParser) + Template-way
+    message = std::string(*file.at<BaseAttribute>("message"));
+    //at (From root ComplexAttribute) + Template-way
+    message = file->at<BaseAttribute>("message")->get<std::string>();
+    //getBaseAttribute + Cast-way
+    message = std::string(*file->getBaseAttribute("message"));
+
+    std::cout << "The message is : " << message << std::endl; //Display the message
 }
