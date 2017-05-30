929a4a8d045afdd282ab043d9cd0f1de325d2e61
Sygmei <Mizugola@gmail.com>
2017-05-30 16:25:56 +0200

    Some API improvements

--- include/vili/NodeValidator.hpp
+++ include/vili/NodeValidator.hpp
@@ -4,15 +4,15 @@
 
 namespace vili
 {
-	template <class T>
-	class NodeValidator : public NodeIterator
-	{
-	private:
-		T m_result;
-	public:
-		NodeValidator();
-		NodeValidator(ComplexAttribute* node);
-		void validate(T result);
-		T result();
-	};
+    template <class T>
+    class NodeValidator : public NodeIterator
+    {
+    private:
+        T m_result;
+    public:
+        NodeValidator();
+        NodeValidator(ComplexAttribute* node);
+        void validate(T result);
+        T result();
+    };
 }
--- include/vili/Types.hpp
+++ include/vili/Types.hpp
@@ -4,34 +4,34 @@
 
 namespace vili
 {
-	namespace Types
-	{
-		enum AttributeType
-		{
-			Attribute,
-			ContainerAttribute,
-			BaseAttribute,
-			ListAttribute,
-			ComplexAttribute,
-			LinkAttribute
-		};
+    namespace Types
+    {
+        enum AttributeType
+        {
+            Attribute,
+            ContainerAttribute,
+            BaseAttribute,
+            ListAttribute,
+            ComplexAttribute,
+            LinkAttribute
+        };
 
-		enum DataType
-		{
-			String,
-			Int,
-			Float,
-			Bool,
-			Range,
-			Link,
-			Template,
-			Unknown
-		};
+        enum DataType
+        {
+            String,
+            Int,
+            Float,
+            Bool,
+            Range,
+            Link,
+            Template,
+            Unknown
+        };
 
-		Types::DataType stringToDataType(const std::string& type);
-		Types::DataType getVarType(const std::string& var);
-		std::string getDefaultValueForType(Types::DataType type);
-		std::string dataTypeToString(Types::DataType type);
-		std::string attributeTypeToString(Types::AttributeType type);
-	}
+        Types::DataType stringToDataType(const std::string& type);
+        Types::DataType getVarType(const std::string& var);
+        std::string getDefaultValueForType(Types::DataType type);
+        std::string dataTypeToString(Types::DataType type);
+        std::string attributeTypeToString(Types::AttributeType type);
+    }
 }
