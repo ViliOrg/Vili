be0df289e310dec6f3691d57681b821233f84551
Sygmei <>
2020-06-04 02:53:04 +0200

    Better strings, better iterators

--- tests/src/string_parsing.cpp
+++ tests/src/string_parsing.cpp
@@ -19,18 +19,12 @@ TEST_CASE("Simple strings", "[vili.parser]")
                     .as<vili::string>()
             == "Hello, world!");
     }
-    SECTION("Multiline")
-    {
-        REQUIRE(vili::parser::from_string(
-                    "multiline: \"Line 1\nLine 2\nLine 3\"")["multiline"]
-                    .as<vili::string>()
-            == "Line 1\nLine 2\nLine 3");
-    }
     SECTION("Escape quotes")
     {
+        std::string quote = "\\\"E = mc^2\\\"";
         REQUIRE(vili::parser::from_string(
-                    "escape: \"Einstein said: \\\"E = mc^2\\\"\"")["escape"]
+                    "escape: \"Einstein said: " + quote + "\"")["escape"]
                     .as<vili::string>()
-            == "Einstein said \"E = mc^2\"");
+            == "Einstein said: " + quote);
     }
 }
