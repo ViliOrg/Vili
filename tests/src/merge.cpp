403fc808430a7e0fbf80963c4e0772fed68f777e
Sygmei <>
2020-06-09 00:46:26 +0200

    Fixed bug when merging not-last container

---
+++ tests/src/merge.cpp
@@ -0,0 +1,17 @@
+#include <catch/catch.hpp>
+
+#include <vili/parser/parser.hpp>
+
+TEST_CASE("Merge")
+{
+    SECTION("Merge and redefine")
+    {
+        vili::node root
+            = vili::parser::from_string("one:{two:{x:1}, z:1}\none:{two:{y:2}, z:3}");
+        REQUIRE(root["one"].size() == 2);
+        REQUIRE(root["one"]["two"].size() == 2);
+        REQUIRE(root["one"]["two"]["x"].as<vili::integer>() == 1);
+        REQUIRE(root["one"]["two"]["y"].as<vili::integer>() == 2);
+        REQUIRE(root["one"]["z"].as<vili::integer>() == 3);
+    }
+}
